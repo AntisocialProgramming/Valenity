10d393eca1584e84262d655a65be8aecf3948245
AntisocialProgramming <90345430+AntisocialProgramming@users.noreply.github.com>
2022-07-03 15:54:43 -0500

    Update AssemblyInfo.cs

--- Valenity/Properties/AssemblyInfo.cs
+++ Valenity/Properties/AssemblyInfo.cs
@@ -5,31 +5,18 @@ using System.Runtime.InteropServices;
 // [+] - Allowed to Change
 // [-] - Not Allowed to Change
 
-
-// [+]
-[assembly: AssemblyTitle("Valenity")]
-// [+]
-[assembly: AssemblyDescription("")]
-// [+]
-[assembly: AssemblyConfiguration("")]
-// [+]
-[assembly: AssemblyCompany("")]
-// [+]
-[assembly: AssemblyProduct("Valenity")]
-// [-]
-[assembly: AssemblyCopyright("Copyright ValenityGames © 2022")]
-// [-]
-[assembly: AssemblyTrademark("")]
-// [+]
-[assembly: AssemblyCulture("")]
-// [+]
-[assembly: ComVisible(false)]
-// [-]
-[assembly: Guid("83f9d8f8-1fc1-4c57-bef0-9b0f33d4ff9e")]
-// [+]
-[assembly: AssemblyVersion("1.0.0.0")]
-// [+]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyTitle("Valenity")] // [+]
+[assembly: AssemblyDescription("")] // [+]
+[assembly: AssemblyConfiguration("")] // [+]
+[assembly: AssemblyCompany("")] // [+]
+[assembly: AssemblyProduct("Valenity")] // [+]
+[assembly: AssemblyCopyright("Copyright ValenityGames © 2022")] // [-]
+[assembly: AssemblyTrademark("")] // [-]
+[assembly: AssemblyCulture("")] // [+]
+[assembly: ComVisible(false)] // [+]
+[assembly: Guid("83f9d8f8-1fc1-4c57-bef0-9b0f33d4ff9e")] // [-]
+[assembly: AssemblyVersion("1.0.0.0")] // [+]
+[assembly: AssemblyFileVersion("1.0.0.0")] // [+]
 
 // REMOVING/ALTERING THE COPYRIGHT AND RIPPING CODE WITHOUT CONSENT WILL GET YOU BLACKLISTED FROM VIEWING MY PAGE IN THE FUTURE.
 // Don't be an ass, just credit me.
